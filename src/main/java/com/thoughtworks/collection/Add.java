f98f6a976ed590ba854a548ef9eb5e2753389b64
mahuanfy <mahuanfy@foxmail.com>
2019-03-25 23:22:18 +0800

    [HuanHuan]: Finish add test

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -2,50 +2,124 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Add {
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        if (leftBorder > rightBorder) {
+            int number = leftBorder;
+            leftBorder = rightBorder;
+            rightBorder = number;
+        }
+        int sum = 0;
+        for (int i = leftBorder; i <= rightBorder; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+
+        return sum;
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        if (leftBorder > rightBorder) {
+            int number = leftBorder;
+            leftBorder = rightBorder;
+            rightBorder = number;
+        }
+        int sum = 0;
+        for (int i = leftBorder; i <= rightBorder; i++) {
+            if (i % 2 != 0) {
+                sum += i;
+            }
+        }
+
+        return sum;
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream()
+                .map(item -> item * 3 + 2)
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i) % 2 != 0) {
+                arrayList.set(i, arrayList.get(i) * 3 + 2);
+            }
+        }
+
+        return arrayList;
     }
 
     public int getSumOfProcessedOdds(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream()
+                .filter(item -> item % 2 != 0)
+                .map(item -> item * 3 + 5)
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 
     public double getMedianOfEven(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        Collections.sort(arrayList);
+        double median;
+        if (arrayList.size() % 2 == 0) {
+            median = (arrayList.get(arrayList.size() / 2 - 1) + arrayList.get(arrayList.size() / 2)) / 2;
+        } else {
+            median = arrayList.get(arrayList.size() / 2);
+        }
+
+        return median;
     }
 
     public double getAverageOfEven(List<Integer> arrayList) {
-        throw new NotImplementedException();
+
+        return arrayList.stream().mapToInt(Integer::byteValue).average().getAsDouble();
     }
 
     public boolean isIncludedInEvenIndex(List<Integer> arrayList, Integer specialElment) {
-        throw new NotImplementedException();
+        return arrayList.stream()
+                .filter(item -> item % 2 == 0)
+                .collect(Collectors.toList())
+                .contains(specialElment);
     }
 
     public List<Integer> getUnrepeatedFromEvenIndex(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        List<Integer> list = new ArrayList<>();
+        arrayList.forEach(item -> {
+            if (!list.contains(item)) {
+                list.add(item);
+            }
+        });
+
+        return list.stream()
+                .filter(item -> item % 2 == 0)
+                .collect(Collectors.toList());
     }
 
     public List<Integer> sortByEvenAndOdd(List<Integer> arrayList) {
-        throw new NotImplementedException();
+
+        Collections.sort(arrayList);
+        List<Integer> list = arrayList.stream().filter(item -> item % 2 == 0).collect(Collectors.toList());
+        Collections.reverse(arrayList);
+        list.addAll(arrayList.stream().filter(item -> item % 2 != 0).collect(Collectors.toList()));
+
+        return list;
     }
 
     public List<Integer> getProcessedList(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < arrayList.size() - 1; i++) {
+            list.add((arrayList.get(i) + arrayList.get(i + 1)) * 3);
+        }
+
+        return list;
     }
 }
