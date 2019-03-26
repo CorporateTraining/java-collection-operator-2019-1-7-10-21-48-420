7d21bdb9233d6962dbadac32c502f3d7b0bfb42a
mahuanfy <mahuanfy@foxmail.com>
2019-03-26 21:25:12 +0800

    [HuanHuan]: Finish Flaten test

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,20 +2,37 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Flaten {
 
     Integer[][] array;
+
     public Flaten(Integer[][] array) {
         this.array = array;
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            list.addAll(Arrays.asList(array[i]));
+        }
+        return list;
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> list = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            list.addAll(Arrays.asList(array[i]));
+        }
+        for (Integer arr : list) {
+            if (!result.contains(arr)) {
+                result.add(arr);
+            }
+        }
+        return result;
     }
 }
