4addb12093c6c15ee7fd0c92692f0b92b2ee358a
mahuanfy <mahuanfy@foxmail.com>
2019-03-26 21:13:34 +0800

    [HuanHuan]: Finish Filter test

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -2,8 +2,10 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Filter {
 
@@ -14,18 +16,31 @@ public class Filter {
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        return array.stream()
+                .filter(item -> item % 2 == 0)
+                .collect(Collectors.toList());
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+        return array.stream()
+                .filter(item -> item % 3 == 0)
+                .collect(Collectors.toList());
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+        List<Integer> list = new ArrayList<>(firstList);
+        list.retainAll(secondList);
+        return list;
     }
 
     public List<Integer> getDifferentElements() {
-        throw new NotImplementedException();
+        List<Integer> list = new ArrayList<>();
+        for(Integer arr : array){
+            if(!list.contains(arr)){
+                list.add(arr);
+            }
+        }
+
+        return list;
     }
 }
